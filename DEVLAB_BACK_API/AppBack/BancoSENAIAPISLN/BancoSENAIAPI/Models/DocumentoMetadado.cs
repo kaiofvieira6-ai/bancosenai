6fcfe42f81e6c915959a78ae5a36c13c92a8504b
fabricioasouza8-gif <kaio.f.vieira6@aluno.senai.br>
2026-09-16 08:56:37 -0300

    feat(documentos): Adição de variaveis não adicionadas anteriormente

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
@@ -0,0 +1,14 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    internal class DocumentoMetadado
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Extensao { get; set; } = string.Empty;
+
+        public string Caminho { get; set; } = string.Empty;
+
+        public int CodigoCliente { get; set; }
+    }
+}
